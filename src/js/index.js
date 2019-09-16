0ded5da64405a0e5150727cc84883e7f00d0dd84
Xacku3d <41637751+Xacku3d@users.noreply.github.com>
2019-09-16 23:02:29 +0300

    desktop version added

---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+const menu = document.querySelector('.menu')
+const bg = document.querySelector('.main')
+const trade = document.querySelector('.trade')
+
+document.querySelector('.menu__trade').addEventListener('click', () => {
+    if (menu.className == 'menu') {
+        menu.className = 'menu hidden'
+        bg.className = 'main secondBG'
+        setTimeout(()=> {
+            if (menu.className !== 'menu') {
+                trade.className = 'trade container show'
+                setTimeout(() => { trade.style.opacity = '1'}, 100)
+            }
+        }, 1000)
+    } else {
+        menu.className = 'menu'
+        bg.className = 'main'
+        trade.className = 'trade container'
+        trade.style.opacity = '0'
+    }
+})
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|ico|svg)$/,
+                use: [ 
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'img/'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts/'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('style.css')
+    ]
+};
