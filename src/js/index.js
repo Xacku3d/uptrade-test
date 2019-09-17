aaf374c55605de89b8a7b781044a109baf185432
Xacku3d <41637751+Xacku3d@users.noreply.github.com>
2019-09-17 16:58:55 +0300

    Trade Block delay on mobiles fixed

--- src/js/index.js
+++ src/js/index.js
@@ -6,14 +6,21 @@ document.querySelector('.menu__trade').addEventListener('click', () => {
     if (menu.className == 'menu') {
         menu.className = 'menu hidden'
         bg.className = 'main secondBG'
-        setTimeout(()=> {
+        if (window.matchMedia('(max-width: 768px)').matches) {
             if (menu.className !== 'menu') {
                 trade.className = 'trade container show'
-                setTimeout(() => { 
-                    trade.style.opacity = '1'
-                }, 100)
+                trade.style.opacity = '1'
             }
-        }, 500)
+        } else {
+            setTimeout(() => {
+                if (menu.className !== 'menu') {
+                    trade.className = 'trade container show'
+                    setTimeout(() => {
+                        trade.style.opacity = '1'
+                    }, 100)
+                }
+            }, 1000)
+        }
     } else {
         menu.className = 'menu'
         bg.className = 'main'
